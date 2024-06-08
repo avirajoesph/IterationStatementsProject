fcee82fa832aca13399bffb8cf2476e748fcd58c
Derrick <Avirajoesph@yahoo.com>
2024-06-08 18:31:17 -0500

    fixed project

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -24,7 +24,7 @@ namespace IterationStatements
             do
             {
                 // Increment num by 1
-
+                num++;
                 // Then add num to the collection - numbers
                 // Hint: reference num inside of the Add method's parentheses
                 numbers.Add(num);
@@ -46,10 +46,13 @@ namespace IterationStatements
             // This is to show the user that the numbers will start increasing on the console
             Console.WriteLine("Increase:");
 
-
+            foreach (var item in numbers)
+            {
+                Console.WriteLine(item);
+            }
             // Create a foreach loop using the collection - numbers
-                //In the scope of the foreach loop, print each number in numbers
-             
+            //In the scope of the foreach loop, print each number in numbers
+
 
 
             Console.WriteLine("");
@@ -57,13 +60,14 @@ namespace IterationStatements
 
 
             // Create a for loop - this will print the numbers in reverse order - from 200 to 1
+           
             // in your initializer set the value of i to 199
             // in your conditional, as long as i is less than or equal to the amount of items in "numbers" - use (numbers.Count)
             // AND as long as i is greater than or equal to 0
             // Decrement i by 1
 
             //start for loop here
-            for (int i = 199; i <= numbers.Count; i++)
+            for (int i = 199; i <= numbers.Count && i >= 0; i--)
             {
                 Console.WriteLine(numbers[i]);
 
